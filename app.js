0f141932cdbef7fedeb506885f5616860a2be395
atory <atory_cc@163.com>
2017-08-12 11:50:46 +0800

    no need path log

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('config');
 const cors = require('koa-cors');
 const session = require('koa-session');
 const winston = require('winston');
-const logger = require('koa-logger');
+// const logger = require('koa-logger');
 
 const profile = require('./profile');
 const schema = require('./schema');
@@ -34,7 +34,7 @@ const CONFIG = {
   rolling: false,
 };
 
-app.use(logger());
+// app.use(logger());
 app.use(loggerMiddleware);
 app.use(session(CONFIG, app));
 
