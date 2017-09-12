25bafdc0f6b2955ea616e300d0cd3aae253312b9
veryci-ttang <guchuantang@verci.com>
2017-09-12 17:20:41 +0800

    histogram should have register

--- app.js
+++ app.js
@@ -71,8 +71,9 @@ const httpRequestDurationMicroseconds = new Prometheus.Histogram({
   name: 'http_request_duration_ms',
   help: 'Duration of HTTP requests in ms',
   labelNames: ['route'],
+  registers: [register],
   // buckets for response time from 0.1ms to 500ms
-  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500],
+  // buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500],
 });
 /** monitor up */
 
