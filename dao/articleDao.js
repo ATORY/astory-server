6a265cf8d201d84b7553472e1626ed759b8faff1
veryci-ttang <guchuantang@verci.com>
2017-09-13 13:36:51 +0800

    articleEdit no draft limit

--- dao/articleDao.js
+++ dao/articleDao.js
@@ -65,9 +65,9 @@ class ArticleDao {
     if (!(articleId instanceof ObjectID)) {
       throw this.Err('articleId should be string');
     }
-    let query = { _id: articleId, draft: false, del: false };
+    let query = { _id: articleId, del: false };
     if (userId && (userId instanceof ObjectID)) {
-      query = { _id: articleId, userId, draft: false, del: false };
+      query = { _id: articleId, userId, del: false };
     }
     if (!this.connected) await this.init();
     const article = await this.Coll.findOne(query);
